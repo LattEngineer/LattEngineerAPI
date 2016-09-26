2b51eaa9e7d2308fa353f0018f7eea673a34cf35
SkaiDream <makedream29@gmail.com>
2016-09-26 09:35:45 +0900

    APIResponse client released

--- src/main/java/io/lattengineer/LattEngineerAPI/client/APIResponser.java
+++ src/main/java/io/lattengineer/LattEngineerAPI/client/APIResponser.java
@@ -1,6 +1,22 @@
 package io.lattengineer.LattEngineerAPI.client;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class APIResponser
 {
-
+	private final URL url;
+	public URL getURL() { return this.url; }
+	
+	public APIResponser(URL apiURL)
+	{
+		if(apiURL == null) throw new NullPointerException("The api url cannot be null");
+		this.url = apiURL;
+	}
+	
+	public APIResponser(String apiURL) throws MalformedURLException
+	{
+		if(apiURL == null || new URL(apiURL) == null) throw new NullPointerException("The api url cannot be null");
+		this.url = new URL(apiURL);
+	}
 }
